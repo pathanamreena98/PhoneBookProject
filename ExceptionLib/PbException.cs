905d80afac1bdc5ca235035453b4533e62351635
pathanamreena98 <69763836+pathanamreena98@users.noreply.github.com>
2021-02-03 21:14:09 +0530

    Add files via upload

---
+++ ExceptionLib/PbException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionLib
+{
+    public class PbException:Exception
+    {
+        public PbException(string errMsg) : base(errMsg)
+        {
+
+        }
+    }
+}
