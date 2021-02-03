d2bc42dbfc263c675de32b22bb5d61d85f493a25
pathanamreena98 <69763836+pathanamreena98@users.noreply.github.com>
2021-02-03 21:10:07 +0530

    Add files via upload

---
+++ BusinessLib/BusinessLayer.cs
@@ -0,0 +1,146 @@
+﻿using EntitiesLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLib;
+using ExceptionLib;
+namespace BusinessLib
+{
+    public class BusinessLayer : IBusiness
+    {
+        /// <summary>
+        /// for adding contact
+        /// </summary>
+        /// <param name="contact"></param>
+        public void AddContact(Contact contact)
+        {
+            try
+            {
+                DataAccess dal = new DataAccess();
+                dal.AddContact(contact);
+            }
+            catch (PbException ex)
+            {
+                throw ex;
+            }
+        }
+        /// <summary>
+        /// for adding category
+        /// </summary>
+        /// <param name="category"></param>
+        public void AddCategory(Category category)
+        {
+            try
+            {
+                DataAccess dal = new DataAccess();
+                dal.AddCategory(category);
+            }
+            catch (PbException ex)
+            {
+                throw ex;
+            }
+        }
+        /// <summary>
+        /// for deleting contact
+        /// </summary>
+        /// <param name="id"></param>
+        public void DeleteContactById(int id)
+        {
+            try
+            {
+                DataAccess dal = new DataAccess();
+                dal.DeleteContactById(id);
+            }
+            catch (PbException ex)
+            {
+                throw ex;
+            }
+        }
+        /// <summary>
+        /// for deleting category by id
+        /// </summary>
+        /// <param name="id"></param>
+        public void DeleteCategoryById(int id)
+        {
+            try
+            {
+                DataAccess dal = new DataAccess();
+                dal.DeleteCategoryById(id);
+            }
+            catch (PbException ex)
+            {
+                throw ex;
+            }
+        }
+        /// <summary>
+        /// for getting all category names
+        /// </summary>
+        /// <returns></returns>
+        public List<Category> GetAllCategoryNames()
+        {
+            try
+            {
+                DataAccess dal = new DataAccess();
+                var lstcategories = dal.GetAllCategoryNames();
+                return lstcategories;
+            }
+            catch (PbException ex)
+            {
+                throw ex;
+            }
+        }
+        /// <summary>
+        /// for getting all contacts
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public List<Contact> GetAllContactsByCId(int id)
+        {
+            try
+            {
+                DataAccess dal = new DataAccess();
+                var lstcontacts = dal.GetAllContactsByCId(id);
+                return lstcontacts;
+            }
+            catch (PbException ex)
+            {
+                throw ex;
+            }
+        }
+        /// <summary>
+        /// for updating contact
+        /// </summary>
+        /// <param name="contact"></param>
+        public void UpdateContactById(Contact contact)
+        {
+            try
+            {
+                DataAccess dal = new DataAccess();
+                dal.UpdateContactById(contact);
+            }
+            catch (PbException ex)
+            {
+                throw ex;
+            }
+        }
+        /// <summary>
+        /// for updating category
+        /// </summary>
+        /// <param name="category"></param>
+        public void UpdateCategoryById(Category category)
+        {
+            try
+            {
+                DataAccess dal = new DataAccess();
+                dal.UpdateCategoryById(category);
+            }
+            catch (PbException ex)
+            {
+                throw ex;
+            }
+        }
+        
+    }
+}
---
+++ BusinessLib/IBusiness.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLib;
+using EntitiesLib;
+
+namespace BusinessLib
+{
+    /// <summary>
+    /// Interface for Business Layer
+    /// </summary>
+    public interface IBusiness
+    {
+        List<Category> GetAllCategoryNames();
+        void AddCategory(Category category);
+        void AddContact(Contact contact);
+        void DeleteContactById(int id);
+        void DeleteCategoryById(int id);
+        List<Contact> GetAllContactsByCId(int id);
+        void UpdateContactById(Contact contact);
+        void UpdateCategoryById(Category category);
+
+    }
+}
